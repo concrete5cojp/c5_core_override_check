9bc116affd6acdc3fd42d8b6923215c5d8463250
Katz Ueno <katz@concrete5.co.jp>
2020-08-25 10:47:36 +0900

    Initial commit

---
+++ c5_core_override_check.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#
+# concrete5 Core Override Checker
+# ----------
+# Version 0.1
+# By katzueno
+#
+# INSTRUCTION:
+# https://github.com/katzueno/c5_core_override_check
+# ----------
+# USE IT AT YOUR OWN RISK!
+# LICENSE: MIT
+
+# Parameters
+DIR_WORKING=$1
+DIR_CONCRETE5=$2
+## Get Download URL and version from https://www.concrete5.org/developers/developer-downloads/
+CONCRETE5_VERSION="8.5.2"
+URL_CONCRETE5="https://www.concrete5.org/download_file/-/view/111592/8497/"
+
+# ==============================
+#
+# DO NOT TOUCH BELOW THIS LINE (unless you know what you're doing.)
+#
+# ==============================
+
+echo " -- -- -- -- -- -- -- -- -- -- --"
+echo "  concrete5 core file checker v 0.1.0"
+echo " -- -- -- -- -- -- -- -- -- -- --"
+echo ""
+echo "Please check the following parameters:"
+echo ""
+echo "Working Directory   : ${DIR_WORKING}"
+echo "concrete5 Directory : ${DIR_CONCRETE5}"
+echo "concrete5 Version   : ${CONCRETE5_VERSION}"
+echo "concrete5 URL       : ${URL_CONCRETE5}"
+echo " -- -- -- -- -- -- -- -- -- -- --"
+echo "[y]. Proceed?"
+echo "[q]. Quit?"
+read -p "Enter your selection:  (y/q): " yesno
+case "$yesno" in [yY]*) ;; *) echo "OK. See you soon!" ; exit 0 ;; esac
+
+while :
+do
+    echo "cd to working directory (doesn't do much)"
+    echo "${DIR_WORKING}"
+    cd ${DIR_WORKING}
+    echo "Copying concrete directory of web to working directory"
+    cp -r ${DIR_CONCRETE5}/concrete concrete5_web
+    echo "Fetching original concrete5 core file"
+    ## wget concrete5-8.5.2-original.zip https://www.concrete5.org/download_file/-/view/111592/8497/
+    wget concrete5-${CONCRETE5_VERSION}-original.zip ${URL_CONCRETE5}
+    echo "Unzipping original concrete5 package to the working directory"
+    ## unzip concrete5-8.5.2-original.zip
+    unzip ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original.zip
+    echo "Moving concrete directory of original package to the working directory"
+    ## mv concrete5-8.5.2/concrete concrete5-8.5.2-original
+    mv ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}/concrete ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original
+    echo "Removing concrete5 original zip file"
+    ## rm concrete5-8.5.2-original.zip
+    rm concrete5-${CONCRETE5_VERSION}-original.zip
+    echo "Now diffing the difference between original package & concrete folder on web server"
+    ## diff -Nurd concrete5-8.5.2-original concrete5_web > concrete5.diff
+    diff -Nurd ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original ${DIR_WORKING}/concrete5_web > ${DIR_WORKING}/concrete5.diff
+    echo "Result:"
+    RESULT=wc -c < ${DIR_WORKING}/concrete5.diff
+    echo "If the result is 0, it's success."
+done
+if [ -s ${DIR_WORKING}/concrete5.diff ]; then
+    echo "********************"
+    echo "WARNING: There is some core override."
+    echo "********************"
+    CURRENT_TIME=$(date "+%Y%m%d%H%M%S")
+    echo "Saving diff file as:"
+    echo "--> ${DIR_WORKING}/concrete5_${CURRENT_TIME}.diff."
+    echo "Make sure to check the diff file!"
+    cp ${DIR_WORKING}/concrete5.diff ${DIR_WORKING}/concrete5_${CURRENT_TIME}.diff
+else
+    echo "Congratulations! No concrete5 core override!"
+fi
+echo " -- -- -- -- -- -- -- -- -- -- --"
+echo "Would you like to delete temporary directory & files?"
+echo " -- -- -- -- -- -- -- -- -- -- --"
+echo "[y]. Yes?"
+echo "[q]. Quit?"
+read -p "Enter your selection:  (y/q): " yesno
+while :
+do
+case "$yesno" in [yY]*) ;; *) echo "Exiting WITHOUT deleting working files. Good bye." ; exit 0 ;; esac
+
+while :
+    echo "Deleting the following temporary files"
+    echo "- ${DIR_WORKING}/concrete5_web"
+    echo "- ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original"
+    echo "- ${DIR_WORKING}/concrete5.diff"
+    rm -r ${DIR_WORKING}/concrete5_web ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original ${DIR_WORKING}/concrete5.diff
+    echo "All Done."
+do
+done
---
+++ readme.md
@@ -0,0 +1,66 @@
+# concrete5 Core Override Checker
+
+concrete5 Core override checker is the very simple interactive tool to check if your concrete5 core directory has some overrides from concrete5 original package.
+
+You need to upload this shell script to the server and run it directory from server (Or download the file to run locally)
+
+## How it works
+
+- It copies the concrete directory from web server to working directory
+- It downloads the concrete5 original package from concrete5.org server & extract the zip file.
+- Run a DIFF command.
+- Check if the diff file size is zero or not.
+- If it's not zero, it gives you warning and save the diff file
+- Deletes the working files.
+
+## Requirement
+
+- You need to have the basic understanding of the server.
+- The server needs to be able to run shell scrpt, and have necessary tools (this shell script uses very basic tools)
+
+## How to set it up
+
+- Open `c5_core_override.chech.sh` file. Make sure you have execution rights.
+- Check the file permission of concrete5 folder.
+- Change the CONCRETE5_VERSION & URL_CONCRETE5 to your preferred version and download link. You can get the download URL from [concrete5.org Download page](https://www.concrete5.org/developers/developer-downloads/]
+- Upload the shell script to the server
+
+## How to run
+
+- Get the working directory URL. I recommend to use under home directory. Make some empty directory, then use `pwd` command to get the path.
+- Get the concrete5 directory. Go to the concrete5 root directory and run `pwd` command.
+- No slash at the end
+
+```
+$ cd [shell script location]
+$ sh c5_core_override_check.sh [path to working directory] [path to concrete5 directory]
+```
+
+### Example command
+
+```
+$ cd
+$ mkdir c5_check
+$ sh c5_core_override_check.sh "/home/ec2-user/c5_check" "/var/www/vhosts/concrete5.co.jp/"
+```
+
+## What to check the result
+
+If there is core override, it will generate concrete5_YYYYMMDDHHMMSS.diff file under working directory.
+
+Download & open the diff file and check the diff file.
+
+# Version History
+
+----|-----|------
+2020/8/25 | 0.9.0 | Initial Version
+
+# Credit
+
+- Katz (concrete5 Japan, Inc.)
+    - [GitHub @katzueno](https://github.com/katzueno)
+    - [Twitter in English @katz515](https://twitter.com/katz515)
+    - [Twitter in Japanese @katzueno](https://twitter.com/katzueno)
+    - [Instagram @katzueno](https://instagram/katzueno)
+    - [Instagram Japanese Curry Rice @katzcurry](https://instagram/katzcurry)
+    - [Web: katzueno.com](https://katzueno.com)
