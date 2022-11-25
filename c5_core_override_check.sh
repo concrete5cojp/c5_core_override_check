7545f0ae6910deae57579b2ad6b7c048a1092c5a
Katz Ueno <katz@macareux.co.jp>
2022-11-25 11:46:41 +0900

    Folder name change support

--- c5_core_override_check.sh
+++ c5_core_override_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# concrete5 Core Override Checker
+# Concrete CMS Core Override Checker
 # ----------
-# Version 0.9.0
+# Version 1.0.0
 # By katzueno
 #
 # INSTRUCTION:
@@ -15,8 +15,8 @@
 DIR_WORKING=$1
 DIR_CONCRETE5=$2
 ## Get Download URL and version from https://www.concrete5.org/developers/developer-downloads/
-CONCRETE5_VERSION="8.5.6"
-URL_CONCRETE5="https://www.concretecms.com/download_file/61dab82f-fb01-47bc-8cf1-deffff890224/9"
+CONCRETE5_VERSION="8.5.12"
+URL_CONCRETE5="https://www.concretecms.org/download_file/2bba0898-1539-48ff-b273-0cbddc7588da"
 
 # ==============================
 #
@@ -25,14 +25,14 @@ URL_CONCRETE5="https://www.concretecms.com/download_file/61dab82f-fb01-47bc-8cf1
 # ==============================
 
 echo " -- -- -- -- -- -- -- -- -- -- --"
-echo "  concrete5 core file checker v 0.9.0"
+echo "  Concrete CMS core file checker v 1.0.0"
 echo " -- -- -- -- -- -- -- -- -- -- --"
 echo "Please check the following parameters:"
 echo ""
 echo "Working Directory   : ${DIR_WORKING}"
-echo "concrete5 Directory : ${DIR_CONCRETE5}"
-echo "concrete5 Version   : ${CONCRETE5_VERSION}"
-echo "concrete5 URL       : ${URL_CONCRETE5}"
+echo "Concrete CMS Directory : ${DIR_CONCRETE5}"
+echo "Concrete CMS Version   : ${CONCRETE5_VERSION}"
+echo "Concrete CMS URL       : ${URL_CONCRETE5}"
 echo " -- -- -- -- -- -- -- -- -- -- --"
 echo "[y]. Proceed?"
 echo "[q]. Quit?"
@@ -45,6 +45,21 @@ do
     break
 done
 
+# https://unix.stackexchange.com/questions/285924/how-to-compare-a-programs-version-in-a-shell-script
+requiredver="8.0.0"
+if [ "$(printf '%s\n' "$requiredver" "${CONCRETE5_VERSION}" | sort -V | head -n1)" = "$requiredver" ]; then 
+  echo "Greater than or equal to ${requiredver}. We are proceeding"
+else
+  echo "Less than ${requiredver}"
+fi
+
+requiredver="8.5.8"
+if [ "$(printf '%s\n' "$requiredver" "${CONCRETE5_VERSION}" | sort -V | head -n1)" = "$requiredver" ]; then 
+  CONCRETE5_PACKAGE_DIRECTORY_NAME="concrete-cms-${CONCRETE5_VERSION}"
+else
+  CONCRETE5_PACKAGE_DIRECTORY_NAME="concrete5-${CONCRETE5_VERSION}"
+fi
+
 echo "cd to working directory (doesn't do much)"
 echo "${DIR_WORKING}"
 cd ${DIR_WORKING}
@@ -52,19 +67,19 @@ echo "Copying concrete directory of web to working directory"
 cp -r ${DIR_CONCRETE5}/concrete concrete5_web
 echo "Fetching original concrete5 core file"
 ## wget concrete5-8.5.2-original.zip https://www.concrete5.org/download_file/-/view/111592/8497/
-wget -O concrete5-${CONCRETE5_VERSION}-original.zip ${URL_CONCRETE5}
+wget -O ${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original.zip ${URL_CONCRETE5}
 echo "Unzipping original concrete5 package to the working directory"
 ## unzip concrete5-8.5.2-original.zip
-unzip ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original.zip
+unzip ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original.zip
 echo "Moving concrete directory of original package to the working directory"
 ## mv concrete5-8.5.2/concrete concrete5-8.5.2-original
-mv ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}/concrete ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original
+mv ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}/concrete ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original
 echo "Removing concrete5 original zip file"
 ## rm concrete5-8.5.2-original.zip
-rm concrete5-${CONCRETE5_VERSION}-original.zip
+rm ${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original.zip
 echo "Now diffing the difference between original package & concrete folder on web server"
 ## diff -Nurd concrete5-8.5.2-original concrete5_web > concrete5.diff
-diff -Nurd ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original ${DIR_WORKING}/concrete5_web > ${DIR_WORKING}/concrete5.diff
+diff -Nurd ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original ${DIR_WORKING}/concrete5_web > ${DIR_WORKING}/concrete5.diff
 echo "Result:"
 wc -c < ${DIR_WORKING}/concrete5.diff
 echo "If the result is 0, it's success."
@@ -86,8 +101,8 @@ echo " -- -- -- -- -- -- -- -- -- -- --"
 echo "Would you like to delete temporary directory & files?"
 echo " -- -- -- -- -- -- -- -- -- -- --"
 echo "- ${DIR_WORKING}/concrete5_web"
-echo "- ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}"
-echo "- ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original"
+echo "- ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}"
+echo "- ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original"
 echo "- ${DIR_WORKING}/concrete5.diff"
 echo " -- -- -- -- -- -- -- -- -- -- --"
 echo "[y]. Yes?"
@@ -102,8 +117,8 @@ done
 
 echo "Deleting the following temporary directory & files"
 echo "- ${DIR_WORKING}/concrete5_web"
-echo "- ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}"
-echo "- ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original"
+echo "- ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}"
+echo "- ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original"
 echo "- ${DIR_WORKING}/concrete5.diff"
-rm -r ${DIR_WORKING}/concrete5_web ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION} ${DIR_WORKING}/concrete5-${CONCRETE5_VERSION}-original ${DIR_WORKING}/concrete5.diff
+rm -r ${DIR_WORKING}/concrete5_web ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME} ${DIR_WORKING}/${CONCRETE5_PACKAGE_DIRECTORY_NAME}-original ${DIR_WORKING}/concrete5.diff
 echo "All Done."
